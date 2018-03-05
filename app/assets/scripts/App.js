22e34fa59a2e3643df58a2df0216f444d9463d66
Theo <pronoygomes@yahoo.com>
2018-03-05 12:29:23 +0000

    completed site header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTax(){
-        let taxAmt = 0;
-        console.log(this.fullName + " owes ££" + taxAmt + " in taxes.");
-    }
-}
-
-var jane = new Adult("Jane", "Orange");
-var jimmy = new Person("Jimmy", "Blue");
-
-jane.greet();
-jane.payTax();
-jimmy.greet();
-
-//$("h1").remove();
+var mobileMenu = new MobileMenu();
